365a41ebb1933dedf91f22cb752e7ef027b35891
Suyog Mule <smulye10@gmail.com>
2024-11-29 23:30:41 +0530

    add: grid layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ProductgridComponent } from './layouts/productgrid/productgrid.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '**',
+    component: ProductgridComponent,
+  },
+];
--- src/app/layouts/productgrid/productgrid.component.ts
+++ src/app/layouts/productgrid/productgrid.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { ProductCardComponent } from '../../components/product-card/product-card.component';
 
 @Component({
   selector: 'app-productgrid',
   standalone: true,
-  imports: [],
+  imports: [ProductCardComponent],
   templateUrl: './productgrid.component.html',
-  styleUrl: './productgrid.component.scss'
+  styleUrl: './productgrid.component.scss',
 })
-export class ProductgridComponent {
-
-}
+export class ProductgridComponent {}
